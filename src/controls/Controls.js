99ec72577495d6cc165f434e53e8ec4d4320d490
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-10 16:41:16 +0200

    Create tests for Controls Component

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -5,7 +5,7 @@ const Controls = props => {
 
   return (
     <div className="controls panel" data-testid="controlsCont">
-      <button data-testid="lockGateButton" disabled={!closed} onClick={toggleLocked} className="toggle-btn" data-testid="lockButton">
+      <button data-testid="lockGateButton" disabled={!closed} onClick={toggleLocked} className="toggle-btn">
         {locked ? 'Unlock Gate' : 'Lock Gate'}
       </button>
       <button data-testid="openGateButton" disabled={locked} onClick={toggleClosed} className="toggle-btn">
--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,17 +1,48 @@
-// Test away!
-/*provide buttons to toggle the closed and locked states.
-buttons' text changes to reflect the state the door will be in if clicked
-the closed toggle button is disabled if the gate is closed
-the locked toggle button is disabled if the gate is open */
+// CONTROLS:
+// provide buttons to toggle the closed and locked states.
+// buttons' text changes to reflect the state the door will be in if clicked
+// the closed toggle button is disabled if the gate is closed
+// the locked toggle button is disabled if the gate is open
 
-// import React from 'react';
-// import * as rt from 'react-testing-library';
-// import Controls from './Controls';
+import React from 'react';
+import * as rt from 'react-testing-library';
+import Controls from './Controls';
+import 'jest-dom/extend-expect';
+afterEach(rt.cleanup);
 
-// // describe('Controls Component', () => {
-// //   it('toggles state of locked button', () => {
-// //     const wrap = rt.render(<Controls locked={true} />);
-// //     rt.fireEvent.click(wrap.queryAllByTestId(/lockButton/i));
-// //     wrap.getAllByText(/unlock/i);
-// //   });
-// // });
+describe('Controls Component', () => {
+  it('provide buttons to toggle the closed state', () => {
+    const wrap = rt.render(<Controls />);
+    wrap.getAllByTestId('openGateButton');
+  });
+  it('provide buttons to toggle the locked state', () => {
+    const wrap = rt.render(<Controls />);
+    wrap.getAllByTestId('lockGateButton');
+  });
+  it('buttons text changes to reflect locked state', () => {
+    const wrap = rt.render(<Controls locked={true} />);
+    expect(wrap.getByTestId('lockGateButton')).toBeDisabled();
+    expect(wrap.getByTestId('lockGateButton')).toHaveTextContent(/unlock/i);
+  });
+  it('buttons text changes to reflect unlocked state', () => {
+    const wrap = rt.render(<Controls locked={false} />);
+    expect(wrap.getByTestId('lockGateButton')).toHaveTextContent(/lock/i);
+  });
+
+  it('buttons text changes to reflect closed state', () => {
+    const wrap = rt.render(<Controls closed={true} />);
+    expect(wrap.getByTestId('openGateButton')).toHaveTextContent(/open/i);
+  });
+  it('buttons text changes to reflect open state', () => {
+    const wrap = rt.render(<Controls closed={false} />);
+    expect(wrap.getByTestId('openGateButton')).toHaveTextContent(/close/i);
+  });
+  it('the closed toggle button is disabled if the gate is locked', () => {
+    const wrap = rt.render(<Controls locked={true} />);
+    expect(wrap.getByTestId('openGateButton')).toBeDisabled();
+  });
+  it('the locked toggle button is disabled if the gate is open', () => {
+    const wrap = rt.render(<Controls closed={false} />);
+    expect(wrap.getByTestId('lockGateButton')).toBeDisabled();
+  });
+});
